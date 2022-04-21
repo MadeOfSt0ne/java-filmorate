8c16be9debbdf8947bcc74c040c500843c201b81
Artur M <artyrillo@yandex.ru>
2022-04-21 16:01:48 +0300

    now extends RuntimeException

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidationException extends Exception {
-    public ValidationException() {
+public class ValidationException extends RuntimeException {
 
-    }
 }
